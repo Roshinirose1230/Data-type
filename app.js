d6441f3b37efc2a0aa3bdcb762476bb7ad15eae5
Roshinirose1230 <67871713+Roshinirose1230@users.noreply.github.com>
2021-06-06 14:02:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+// PRIMITIVE
+
+// String
+const name = 'John Doe';
+console.log(typeof name);
+// Number
+const age = 45;
+console.log(typeof age);
+// Boolean
+const hasKids = true;
+console.log(typeof haskids);
+// Null
+const car = null;
+console.log(typeof car);
+// Undefined
+let test;
+console.log(typeof test);
+// Symbol
+const sym = Symbol();
+console.log(typeof sym);
+
+// REFERENCE TYPES - Objects
+// Array
+const hobbies = ['movies', 'music'];
+console.log(typeof hobbies);
+// Object literal
+const address = {
+  city: 'Boston',
+  state: 'MA'
+}
+console.log(typeof address);
+
+//find day and date using object literal
+const today = new Date();
+console.log(today);
+console.log(typeof today);
